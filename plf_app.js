5cbb87c8b90a137f237466360f7b6f6f555762ba
ya-sukesuke <kefudao7@gmail.com>
2025-12-04 13:24:31 +0900

    back

--- plf_app.js
+++ plf_app.js
@@ -6,3 +6,21 @@ document.getElementById("imgInput").addEventListener("change", function (e) {
     preview.src = URL.createObjectURL(file);
     preview.classList.remove("hidden");
 });
+
+const ageSelect = document.getElementById("ageSelect");
+
+for (let age = 0; age <= 35; age++) {
+  const option = document.createElement("option");
+  option.value = age;
+  option.textContent = `${age}歳`;
+  ageSelect.appendChild(option);
+}
+
+const manthSelect = document.getElementById("manthSelect");
+
+for (let manth = 0; manth <= 11; manth++) {
+  const option = document.createElement("option");
+  option.value = manth;
+  option.textContent = `${manth}ヶ月`;
+  manthSelect.appendChild(option);
+}
